bdad1c6de2eeb4e337f4fb89526c173ec3b8cce8
kevin schuchard <schuchard.kevin@gmail.com>
2019-12-06 15:19:46 -0700

    purgecss content for multi project repos

--- packages/tailwind-schematic/src/files/root/webpack-config.js
+++ packages/tailwind-schematic/src/files/root/webpack-config.js
@@ -1,6 +1,6 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
-  content: ['./src/**/*.html', './src/**/*.component.ts'],
+  content: ['./**/src/**/*.html', './**/src/**/*.component.ts'],
   // Include any special characters you're using in this regular expression
   defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
 });
