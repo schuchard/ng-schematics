ea242561b2841166688694f6b35aec0b8b681835
kevin schuchard <schuchard.kevin@gmail.com>
2019-12-05 21:03:32 -0700

    redo sandbox app

--- sandbox/test-app/src/app/app.component.spec.ts
+++ sandbox/test-app/src/app/app.component.spec.ts
@@ -5,8 +5,12 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent]
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   }));
 
@@ -16,18 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sandbox'`, () => {
+  it(`should have as title 'test-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('sandbox');
+    expect(app.title).toEqual('test-app');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to sandbox!'
-    );
+    expect(compiled.querySelector('.content span').textContent).toContain('test-app app is running!');
   });
 });
