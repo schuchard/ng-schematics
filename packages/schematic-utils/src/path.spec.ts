279bb0d5a911f0223f1ddf059304a1cafbcbf955
kevin schuchard <schuchard.kevin@gmail.com>
2019-12-06 22:37:51 -0700

    add test

--- packages/schematic-utils/src/path.spec.ts
+++ packages/schematic-utils/src/path.spec.ts
@@ -2,6 +2,7 @@ import test from 'ava';
 import { parsePath } from './path';
 
 test('parseName: should return path info', (t) => {
+  t.deepEqual(parsePath(''), { dirname: '', path: '', filename: '' });
   t.deepEqual(parsePath('a'), { dirname: '', path: 'a', filename: 'a' });
   t.deepEqual(parsePath('a/'), { dirname: '', path: 'a', filename: 'a' });
   t.deepEqual(parsePath('a/b'), { dirname: 'a', path: 'a/b', filename: 'b' });
