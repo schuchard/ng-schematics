8e4f944d69ca249cecbd4844e0031a9fe416fa2b
kevin schuchard <schuchard.kevin@gmail.com>
2019-12-06 23:09:58 -0700

    use sh scripts

---
+++ packages/tailwind-schematic/scripts/launch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "linking sandbox"
+yarn link:sandbox
+
+echo "executing schematic"
+cd ../../sandbox/test-workspace &&
+yarn &&
+./node_modules/.bin/ng add @schuchard/tailwind-schematic
