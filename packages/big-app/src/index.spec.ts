7d325975015af6c905e9b00a566f5606d2d8edb0
kevin schuchard <schuchard.kevin@gmail.com>
2020-01-29 07:53:37 -0700

    unit tests

--- packages/big-app/src/index.spec.ts
+++ packages/big-app/src/index.spec.ts
@@ -4,9 +4,7 @@ import { JsonObject } from '@angular-devkit/core';
 import { generateIdentifiers } from './index';
 
 test('scaffolds the base files', async (t) => {
-  const { files } = await runSchematic({ modules: '3', components: '3' });
-
-  // console.log(files);
+  const { files } = await runSchematic({ modules: '3', components: '4' });
 
   t.assert(files.includes('/angular.json'));
   t.assert(files.includes('/projects/app-one/src/app/app.module.ts'));
@@ -14,11 +12,37 @@ test('scaffolds the base files', async (t) => {
   t.assert(files.includes('/projects/lib-one/src/lib/lib-one.module.ts'));
 });
 
-async function runSchematic(options: JsonObject = {}, command = 'ng-add'): Promise<UnitTestTree> {
-  const schematicRunner = new SchematicTestRunner('big-app', require.resolve('../collection.json'));
+test('adds each module', async (t) => {
+  t.plan(9);
 
-  return await schematicRunner.runSchematicAsync(command, options).toPromise();
-}
+  const { files } = await runSchematic({ modules: '3', components: '4' });
+
+  ['a', 'b', 'c'].forEach((mod) => testModuleFiles(mod));
+
+  function testModuleFiles(mod: string) {
+    t.assert(files.includes(`/projects/app-one/src/app/${mod}-mod/${mod}-mod-routing.module.ts`));
+    t.assert(files.includes(`/projects/app-one/src/app/${mod}-mod/${mod}-mod.module.ts`));
+    t.assert(files.includes(`/projects/app-one/src/app/${mod}-mod/${mod}-mod.component.ts`));
+  }
+});
+
+test('adds components to each module', async (t) => {
+  t.plan(12);
+
+  const { files } = await runSchematic({ modules: '3', components: '4' });
+
+  ['a', 'b', 'c'].forEach((mod) => {
+    ['a', 'b', 'c', 'd'].forEach((comp) => hasComponentFile(mod, comp));
+  });
+
+  function hasComponentFile(mod: string, comp: string) {
+    t.assert(
+      files.includes(
+        `/projects/app-one/src/app/${mod}-mod/${mod}-mod-${comp}-comp/${mod}-mod-${comp}-comp.component.ts`
+      )
+    );
+  }
+});
 
 test('generate names should create unique ids a-mod', (t) => {
   t.is(generateIdentifiers(100, 'mod')[0], 'a-mod');
@@ -35,3 +59,9 @@ test('generate names should create unique ids aa-mod', (t) => {
 test('generate names should create unique ids af-mod', (t) => {
   t.is(generateIdentifiers(100, 'mod')[52], 'ba-mod');
 });
+
+async function runSchematic(options: JsonObject = {}, command = 'ng-add'): Promise<UnitTestTree> {
+  const schematicRunner = new SchematicTestRunner('big-app', require.resolve('../collection.json'));
+
+  return await schematicRunner.runSchematicAsync(command, options).toPromise();
+}
